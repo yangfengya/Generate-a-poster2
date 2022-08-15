fc2a46ddcb19c62823da54331c2da37497a9e077
杨锋 <577637883@qq.com>
2022-08-15 21:04:12 +0800

    截图demo

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+// 开放静态文件
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+app.listen('80')
